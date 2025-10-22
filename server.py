5a2a0fe357feb7929541c0222a4f469a4db7c933
budyanskaya <jene_26@mail.ru>
2025-10-22 10:06:11 +0300

    server.py

--- server.py
+++ server.py
@@ -3,15 +3,17 @@ from concurrent import futures
 import inventory_pb2
 import inventory_pb2_grpc
 
+# Создаем класс, который обрабатывает запросы от клиентов
 class InventoryControlServicer(inventory_pb2_grpc.InventoryControlServicer):
+    # Вызываем этот метод, когда клиент отправляет нам товары
     def BulkUpdateStock(self, request_iterator, context):
         print("Начало обработки потока обновлений от клиента...")
         items_processed = 0
-
+        # По одному принимаем товары, которые присылает клиент
         for stock_item in request_iterator:
             items_processed += 1
             print(f"Обработан товар: ID={stock_item.item_id}, Название='{stock_item.name}', Новое количество={stock_item.new_quantity}")
-
+        # Когда все товары получены, отправляем клиенту итоговый ответ
         summary = inventory_pb2.BulkUpdateSummary(
             items_processed=items_processed,
             message=f"Успешно обновлено {items_processed} позиций.",
@@ -21,7 +23,10 @@ class InventoryControlServicer(inventory_pb2_grpc.InventoryControlServicer):
         return summary
 
 def serve():
+    # Создаём сервер, который может работать с 10 клиентами одновременно
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
+    
+    # Определяем какой класс серверу надо использовать для обработки запросов
     inventory_pb2_grpc.add_InventoryControlServicer_to_server(
         InventoryControlServicer(), server
     )
@@ -31,4 +36,5 @@ def serve():
     server.wait_for_termination()
 
 if __name__ == '__main__':
+
     serve()
