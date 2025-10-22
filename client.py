bdc8f95d09894aaef9d3befa9651c00d6901ae6c
budyanskaya <jene_26@mail.ru>
2025-10-22 10:19:17 +0300

    client.py

--- client.py
+++ client.py
@@ -2,12 +2,14 @@ import grpc
 import inventory_pb2
 import inventory_pb2_grpc
 
+# Функция, которая создаёт товары для отправки
 def generate_stock_updates():
     stock_updates = [
         {"item_id": "SKU001", "name": "Ноутбук", "new_quantity": 15},
         {"item_id": "SKU002", "name": "Монитор", "new_quantity": 8},
     ]
-    
+
+    # По одному создаём и отправляем каждый товар
     for item in stock_updates:
         yield inventory_pb2.StockItem(
             item_id=item["item_id"],
@@ -17,11 +19,14 @@ def generate_stock_updates():
 
 def run():
     with grpc.insecure_channel('localhost:50051') as channel:
+        # Создаём объект для вызова методов сервера
         stub = inventory_pb2_grpc.InventoryControlStub(channel)
         print("Отправка данных серверу...")
-        
+
+        # Вызываем метод сервера и передаём ему наши товары
         response = stub.BulkUpdateStock(generate_stock_updates())
         print(f"Результат: {response.message}")
 
 if __name__ == '__main__':
+
     run()
